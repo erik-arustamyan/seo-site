6205ba865090b6b3c20dcd7df82e14da44a0e1dd
erik-arustamyan <30750213+erik-arustamyan@users.noreply.github.com>
2017-08-20 16:06:40 +0400

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+$('#Buy').click(function(){
+	$('.Buy_Popup').css({'display':'block'});
+});
+$('.bg').click(function(){
+	$('.Buy_Popup').css({'display':'none'});
+
+	
+});
+});
+
